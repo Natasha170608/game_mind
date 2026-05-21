abc09d48da02959ff6214921de9e8e92a89f765b
Polinako267 <polina.timonova07@mail.ru>
2026-05-21 15:52:44 +0300

    Изменила файл чтобы было независимо от масштаба экрана

--- Program.cs
+++ Program.cs
@@ -2,14 +2,12 @@ namespace WinFormsApp12
 {
     internal static class Program
     {
-        
         [STAThread]
         static void Main()
         {
+            Application.SetHighDpiMode(HighDpiMode.DpiUnaware);
             ApplicationConfiguration.Initialize();
             Application.Run(new QuizeGame());
-
-
         }
     }
 }
