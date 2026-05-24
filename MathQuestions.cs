6d1328992cb8ebb2ecbe6dfe063592e07a2d90d2
Polinako267 <polina.timonova07@mail.ru>
2026-05-24 23:16:16 +0300

    Сделала вопросы и ответы для математики, загадок и 2 правды 1 ложь

---
+++ MathQuestions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WinFormsApp12;
+
+namespace QuizeGame
+{
+    public class MathQuestions
+    {
+        public static List<QuestionData> GetQuestions()
+        {
+            return new List<QuestionData>
+            {
+                new QuestionData("Сколько будет 2 + 2?", new string[] { "3", "4", "5" }, 1),
+                new QuestionData("Сколько будет 5 + 7?", new string[] { "10", "12", "13" }, 1),
+                new QuestionData("Сколько будет 9 - 4?", new string[] { "5", "6", "4" }, 0),
+                new QuestionData("Сколько будет 3 * 4?", new string[] { "12", "10", "14" }, 0),
+                new QuestionData("Сколько будет 15 : 3?", new string[] { "5", "6", "4" }, 0),
+                new QuestionData("Сколько будет 30 : 3?", new string[] { "5", "1", "10" }, 2),
+                new QuestionData("Сколько будет 9 + 6?", new string[] { "14", "15", "16" }, 1),
+                new QuestionData("Сколько будет 15 - 7?", new string[] { "7", "9", "8" }, 2),
+                new QuestionData("Сколько будет 12 * 2?", new string[] { "24", "34", "26" }, 0),
+                new QuestionData("Сколько будет 52 : 4?", new string[] { "17", "19", "13" }, 2)
+            };
+        }
+    }
+}
---
+++ RiddleQuestions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WinFormsApp12;
+
+namespace QuizeGame
+{
+    public class RiddleQuestions
+    {
+        public static List<QuestionData> GetQuestions()
+        {
+            return new List<QuestionData>
+            {
+                new QuestionData("У какого преступника есть совесть?",
+                    new string[] { "вор", "убийца", "мошенник" }, 1),
+
+                new QuestionData("Что делает сторож, когда у него на шапке сидит воробей?",
+                    new string[] { "спит", "кричит", "машет" }, 0),
+
+                new QuestionData("Чей голос звучит громче всего в лесу?",
+                    new string[] { "дятла", "волка", "эха" }, 2),
+
+                new QuestionData("Что можно сломать, даже не прикасаясь?",
+                    new string[] { "палку", "обещание", "тишину" }, 1),
+
+                new QuestionData("Что служит для обмена информации между людьми, но не имеет ни звука, ни текста?",
+                    new string[] { "взгляд", "интернет", "жесты" }, 0),
+
+                new QuestionData("Чем можно поделиться только с тем, у кого этого нет?",
+                    new string[] { "деньги", "секрет", "еда" }, 1),
+
+                new QuestionData("Что нельзя купить ни за какие деньги, но можно подарить?",
+                    new string[] { "любовь", "совет", "счастье" }, 1),
+
+                new QuestionData("Что не имеет веса, но может заставить человека упасть?",
+                    new string[] { "ветер", "мысль", "сон" }, 2),
+
+                new QuestionData("Что есть у каждого человека, но у каждого своё, и это нельзя потрогать?",
+                    new string[] { "имя", "прошлое", "характер" }, 0),
+
+                new QuestionData("У кого есть спинка, но нет живота?",
+                    new string[] { "кресло", "диван", "стул" }, 2)
+            };
+        }
+    }
+}
