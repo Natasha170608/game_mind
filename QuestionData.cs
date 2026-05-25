0b8e6c817d91b94f3b2fd09c2167e6202d6702b3
Polinako267 <polina.timonova07@mail.ru>
2026-05-25 13:45:42 +0300

    Сделала структуру для одного вопроса

---
+++ QuestionData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizeGame
+{
+    public class QuestionData
+    {
+        public string Text { get; set; }
+        public string[] Options { get; set; }
+        public int CorrectOptionIndex { get; set; }
+
+        public QuestionData(string text, string[] options, int correctOptionIndex)
+        {
+            Text = text;
+            Options = options;
+            CorrectOptionIndex = correctOptionIndex;
+        }
+    }
+}
